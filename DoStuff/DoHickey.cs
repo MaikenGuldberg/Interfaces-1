760683c30c4177bb4ce0e5ddcf7cd01fb6113809
Maiken Guldberg <maiken.g@fibermail.dk>
2019-01-26 13:07:50 +0100

    changes

---
+++ DoStuff/DoHickey.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace DoStuff
+{
+    public class DoHickey: IDoThings
+    {
+        public void DoNothing()
+        {
+            Console.WriteLine(this.GetType().Name + ": :" + System.Reflection.MethodBase.GetCurrentMethod().Name);
+        }
+
+        public int DoSomething(int number)
+        {
+            Console.WriteLine(this.GetType().Name + ": :" + System.Reflection.MethodBase.GetCurrentMethod().Name + ": " + number.ToString());
+            return number;
+        }
+
+        public string DoSomethingElse(string input)
+        {
+            Console.WriteLine(this.GetType().Name + ": :" + System.Reflection.MethodBase.GetCurrentMethod().Name + ": " + input);
+            return input;
+        }
+    }
+}
--- Interfaces-1/Program.cs
+++ Interfaces-1/Program.cs
@@ -3,13 +3,25 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using DoStuff;
 
-namespace Interfaces_1
+namespace DoStuff.Application
 {
     class Program
     {
         static void Main(string[] args)
         {
+            IDoThings myStuff = new DoHickey();
+            Console.WriteLine("Do you want to do dickey or hickey?\nIf dickey type in d or if hickey type in h:");
+            string input = Console.ReadLine();
+            if (input.Equals("d"))
+            {
+                myStuff = new DoDickey();
+            }
+            myStuff.DoNothing();
+            myStuff.DoSomething(3);
+            myStuff.DoSomethingElse("Hallo world");
+            Console.ReadLine();
         }
     }
 }
